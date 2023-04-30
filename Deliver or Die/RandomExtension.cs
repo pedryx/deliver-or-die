e6c82eb66b9b2d3df428d1753f774898fb8dd509
Petr Kotáb <petrkotab99@gmail.com>
2023-04-30 17:42:43 +0200

    Random angle generation.

---
+++ Deliver or Die/RandomExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DeliverOrDie;
+internal static class RandomExtension
+{
+    /// <summary>
+    /// Generate random angle in radians in range [0; 2pi).
+    /// </summary>
+    public static float NextAngle(this Random random)
+        => random.NextSingle() * 2 * MathF.PI;
+}
